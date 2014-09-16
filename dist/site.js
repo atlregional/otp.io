30c581e0323655f0af314ed8cea18e5b9348202b
Landon Reed <landontreed@gmail.com>
2014-09-16 11:35:28 -0400

    updated isochrones

--- dist/site.js
+++ dist/site.js
@@ -33642,7 +33642,15 @@ module.exports.bboxify = function(context) {
 };
 
 module.exports.otpify = function(context) {
-    context.data.set({ map: otpify(context.data.get('map')) });
+    // context.data.set({ map: otpify(context.data.get('map')) });
+    var features = context.data.get('map').features;
+    console.log(features);
+    $.each(features, function(i, feature){
+      if (feature.geometry.type === 'Point'){
+        feature.properties.url = 'http://opentrip.atlantaregion.com/otp-leaflet-client/?module=planner&fromPlace='+feature.geometry.coordinates[1]+','+feature.geometry.coordinates[0]+'&toPlace='+features[features.length - 1].geometry.coordinates[1]+','+features[features.length - 1].geometry.coordinates[0]+'&mode=TRANSIT%2CWALK&maxWalkDistance=3218.688&arriveBy=false&showIntermediateStops=false&itinIndex=0';
+      }
+    });
+    context.data.set({});
 };
 var request = require('request');
 var getJsonFromJsonP = function (url, callback) {
@@ -33668,12 +33676,13 @@ request(url, function (error, response, body) {
   }
 })
 }
-
 module.exports.isochrones = function(context) {
 	console.log(context.data.get('map'));
-  var features = context.data.get('map').features;
+  var map = context.data.get('map')
+  // var features = map.features;
   var isochrones = [];
   var geojson;
+  var features = [];
   var geoLayer;
 	var options = {
 	  host: 'opentrip.atlantaregion.com',
@@ -33681,55 +33690,49 @@ module.exports.isochrones = function(context) {
 	  path: '/otp-rest-servlet/ws/plan?&fromPlace=33.87725673930016%2C-84.46014404296875&toPlace=33.74946419232578%2C-84.38873291015625&time=1%3A13pm&date=03-21-2014&mode=TRANSIT%2CWALK&maxWalkDistance=750&arriveBy=false&showIntermediateStops=false&itinIndex=0&callback=?',
 	  // headers: {'dataType': 'jsonp'}
 	};
-// 	getJsonFromJsonP('http://www.linkedin.com/countserv/count/share?url=http://techcrunch.com/2012/01/29/turning-two-founderscard-pulls-back-the-curtain-on-its-membership-community-for-entrepreneurs/', function (err, data) {
-//     console.log('data count', data.count);
-// });
-  $.each(features, function(i, feature){
-    var url = 'http://opentrip.atlantaregion.com' + '/otp-rest-servlet/ws/isochrone?algorithm%20=accSampling&fromPlace='+ feature.geometry.coordinates[1] + ',' + feature.geometry.coordinates[0]+'&date=2014/06/16&time=12:00:00&maxWalkDistance=1000&mode=WALK,TRANSIT&cutoffSec=1800';
-    $.ajax({
-        url: url,
-        type: "GET",
-        dataType: "jsonp", 
-        success: function( data ) {
-          geojson = data;
-          geoLayer = L.geoJson(geojson, {
+  console.log(context)
+    $.each(map.features, function(i, feature){
+      var url = 'http://opentrip.atlantaregion.com' + '/otp-rest-servlet/ws/isochrone?algorithm%20=accSampling&fromPlace='+ feature.geometry.coordinates[1] + ',' + feature.geometry.coordinates[0]+'&date=2014/06/16&time=12:00:00&maxWalkDistance=1000&mode=WALK,TRANSIT&cutoffSec=1800';
+      $.ajax({
+          url: url,
+          type: "GET",
+          dataType: "jsonp", 
+          success: function( data ) {
+            geojson = data;
+            features.push(geojson.features);
+            // geoLayer = L.geoJson(geojson, {
 
-            style: function (feature) {
-              switch (feature.properties.Time) {
-                      case 1800: return {fillColor: "#ff0000", fillOpacity: 0.1, weight: 0};
-                      // case 3600:   return {fillColor: "#0000ff", weight: 0};
-                  }
-            },
+            //   style: function (feature) {
+            //     switch (feature.properties.Time) {
+            //             case 1800: return {fillColor: "#ff0000", fillOpacity: 0.1, weight: 0};
+            //             // case 3600:   return {fillColor: "#0000ff", weight: 0};
+            //         }
+            //   },
 
-            // onEachFeature: onEachFeature,
+            //   // onEachFeature: onEachFeature,
 
-            pointToLayer: function (feature, latlng) {
-              return L.circleMarker(latlng, {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-              });
-            }
-          }).addTo(window.api.map);
-          // console.log(data)
-        }
+            //   pointToLayer: function (feature, latlng) {
+            //     return L.circleMarker(latlng, {
+            //       radius: 8,
+            //       fillColor: "#ff7800",
+            //       color: "#000",
+            //       weight: 1,
+            //       opacity: 1,
+            //       fillOpacity: 0.8
+            //     });
+            //   }
+            // }).addTo(window.api.map);
+
+            console.log(data)
+            context.data.mergeFeatures(geojson.features, 'map');
+          }
+      });
+      // if (i === map.features.length -1){
+      //   console.log(features)
+      //   context.data.mergeFeatures(features, 'map');
+      // }
     });
-  });
-	
-
-	// http.get(options, function(res) {
-	//   console.log("Got response: " + res.statusCode);
-
-	//   res.jsonp("data", function(chunk) {
-	//     console.log("BODY: " + chunk);
-	//   });
-	// }).on('error', function(e) {
-	//   console.log("Got error: " + e.message);
-	// });
-    // context.data.mergeFeatures(isochrones(context.data.get('map')), 'meta');
+    
 };
 },{"geojson-extent":64,"geojson-random":71,"http":29,"request":96}],196:[function(require,module,exports){
 module.exports = function(context) {
